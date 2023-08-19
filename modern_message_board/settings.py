39be9f5cf727db8359b57f1f1eb4fcd989fa9965
Tadhg N <tadhgnolan@gmail.com>
2023-08-19 16:48:29 +0000

    Add to allowed hosts

--- modern_message_board/settings.py
+++ modern_message_board/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['modernmb.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['modernmb.herokuapp.com', 'localhost', 'modernmb-45f157023587.herokuapp.com']
 
 
 # Application definition
