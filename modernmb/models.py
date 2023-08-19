426e93f8c35a39eff3c6944120dc6615e9fd717b
Tadhg N <tadhgnolan@gmail.com>
2023-08-19 21:07:03 +0000

    Add models and forms to modernmb

--- modernmb/models.py
+++ modernmb/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+
+    category = models.ForeignKey(Category, on_delete=SET_NULL)
+
+    def __str__(self):
+        return self.title
