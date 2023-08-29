7a4e03802145b8d10354ef4ab6d14b46f58d8378
Tadhg N <tadhgnolan@gmail.com>
2023-08-29 00:17:08 +0000

    Correct path errors in modernmb/models.py

--- modernmb/urls.py
+++ modernmb/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('post/<int:id>', views.view_post, name='view_post'),
+    path('', views.get_posts, name='posts'),
+    path('post/<int:id>/', views.view_post, name='view_post'),
     path('posts/add/', views.add_post, name='add_post'),
-    path('posts/update/<int:id>', views.update_post, name='update_post'),
-    path('posts/delete/<int:id>', views.delete_post, name='delete_post'),
+    path('posts/update/<int:id>/', views.update_post, name='update_post'),
+    path('posts/delete/<int:id>/', views.delete_post, name='delete_post'),
     path('categories/', views.categories, name='categories'),
     path('category/add/', views.add_category, name='add_category'),
-    path('category/update/<int:id>', views.update_category, name='update_category'),
-    path('category/delete/<int:id>', views.delete_category, name='delete_category'),
+    path('category/update/<int:id>/', views.update_category, name='update_category'),
+    path('category/delete/<int:id>/', views.delete_category, name='delete_category'),
 ]
